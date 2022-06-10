e9f37c3f00cb98f23703070f3e9b9c51d1f35f95
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-10 16:09:01 -0400

    test:validations testing for user prop and testhole

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
   has_many :user_test_holes
   has_many :test_holes, through: :user_test_holes
+
+    validates_presence_of :city
+    validates_presence_of :name
+    validates_presence_of :address
+    validates_presence_of :phone
+    validates_presence_of :email
+    validates_presence_of :inspector
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,12 @@ RSpec.describe User, type: :model do
     it { should have_many(:user_test_holes) }
     it { should have_many(:test_holes).through(:user_test_holes) }
   end
+  describe 'validations' do
+    it { should validate_presence_of(:city)}
+    it { should validate_presence_of(:address)}
+    it { should validate_presence_of(:phone)}
+    it { should validate_presence_of(:email)}
+    it { should validate_presence_of(:inspector)}
+    it { should validate_presence_of(:name)}
+  end
 end
