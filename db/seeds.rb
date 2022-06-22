9ad6647634b7104e8c8e01b8e793bbade14c69ed
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-21 21:25:32 -0400

    feat:add error message to contractor index and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'Rich',
+             city: 'Cumberland',
+             address: '123 fake st',
+             phone: '123456',
+             email: 'email@email.com',
+             inspector: false)
+User.create!(name: 'Charlie',
+             city: 'Cumberland',
+             address: '111  different fake st',
+             phone: '123456345',
+             email: 'email22@email.com',
+             inspector: false)
