41a133bb0638a40ba16bb9ba3ee8103b687048b7
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-24 13:47:08 -0400

    feat: migrate password_digest and has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 class User < ApplicationRecord
   has_many :user_test_holes
   has_many :test_holes, through: :user_test_holes
+  has_secure_password
 
   validates_presence_of :city
   validates_presence_of :name
