d92b9caf0605703a27ec6b2976f53decc4f3ca46
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-25 13:06:13 -0400

    feat: add create contractors route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :contractors, only: %i[index show]
+      resources :contractors, only: %i[index show create]
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
