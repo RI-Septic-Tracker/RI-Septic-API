e5c87c1c00eaeba12020dc34a82b4ecd65f5b5ae
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-25 12:45:14 -0400

    feat: rubocop appeasement

--- db/migrate/20220624174142_add_password_to_user.rb
+++ db/migrate/20220624174142_add_password_to_user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# adds password to user table for bcrypt
 class AddPasswordToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :password_digest, :string
