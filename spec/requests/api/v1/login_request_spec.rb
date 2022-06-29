082ce2b74282867a0daab4da8b57888dd416659f
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-29 16:29:37 -0600

    login stuff updated

--- spec/requests/api/v1/login_request_spec.rb
+++ spec/requests/api/v1/login_request_spec.rb
@@ -1,17 +1,22 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe "UsersController" do
+  before(:each) do
+   @user = User.create!(name: 'Rich',
+      city: 'Cumberland',
+      address: '123 fake st',
+      phone: '123456',
+      email: 'email@email.com',
+      role: 'contractor',
+      password: "password", 
+      password_confirmation: "password")
+  end
+
   it "creates a new user", :vcr do
     user_params = {
-                   name: 'Rich',
-                   city: 'Cumberland',
-                   address: '123 fake st',
-                   phone: '123456',
-                   email: 'email@email.com',
-                   role: 'contractor', password: "password", password_confirmation: "password"
-      # "email": "me@email.com",
-      # "password": "12345",
-      # "password_confirmation": "12345"
+                  'email': 'email@email.com',
+                  'password': "password"
     }
     headers = {
       'Content-Type' => 'application/json',
@@ -44,7 +49,7 @@ RSpec.describe "UsersController" do
     parsed_data = JSON.parse(response.body, symbolize_names: true)
 
     expect(parsed_data[:status]).to eq(400)
-    expect(parsed_data[:message]).to eq("Password can't be blank, City can't be blank, Name can't be blank, Address can't be blank, and Phone can't be blank")
+    expect(parsed_data[:message]).to eq("City can't be blank, Name can't be blank, Address can't be blank, Phone can't be blank, and Email can't be blank")
   end
 
   it "sad paths no pw provided" do
