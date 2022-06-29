efd73f8cbd628a5a106276a1ed95d00ab3255c4c
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-29 12:21:18 -0400

    feat: happy path: creates property, no access control' '

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,6 +1,13 @@
 module Api
   module V1
     class PropertiesController < ApplicationController
+      def create
+        Property.create!(property_params)
+      end
+      private
+      def property_params
+        params.require(:property).permit(:city, :street, :num, :bedrooms)
+      end
     end
   end
 end
