17c5b29e1eb3746407714cddd0dfd9262c0dd2f2
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-29 12:42:33 -0400

    test: serialized return from post request

--- spec/requests/api/v1/property_request_spec.rb
+++ spec/requests/api/v1/property_request_spec.rb
@@ -16,7 +16,16 @@ RSpec.describe 'Property Requests' do
       headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json'}
       post '/api/v1/properties', headers: headers, params: JSON.generate(data)
       reply = JSON.parse(response.body, symbolize_names: true)
-      binding.pry
+      expect(reply).to be_a Hash
+      expect(reply).to have_key(:data)
+      expect(reply[:data]).to have_key(:id)
+      expect(reply[:data]).to have_key(:type)
+      expect(reply[:data]).to have_key(:attributes)
+      expect(reply[:data][:type]).to eq("property")
+      expect(reply[:data][:attributes]).to have_key(:city)
+      expect(reply[:data][:attributes]).to have_key(:street)
+      expect(reply[:data][:attributes]).to have_key(:num)
+      expect(reply[:data][:attributes]).to have_key(:bedrooms)
     end
   end
 end
