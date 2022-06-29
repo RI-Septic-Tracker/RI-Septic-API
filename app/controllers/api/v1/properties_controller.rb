4e5111ae5620b020d66e2a3e462cdd23c158c2f1
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-06-29 12:44:18 -0400

    appease robocop

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,11 +1,16 @@
+# frozen_string_literal: true
+
 module Api
   module V1
+    # controller for property DB
     class PropertiesController < ApplicationController
       def create
         prop = Property.create!(property_params)
         render json: PropertySerializer.new(prop)
       end
+
       private
+
       def property_params
         params.require(:property).permit(:city, :street, :num, :bedrooms)
       end
