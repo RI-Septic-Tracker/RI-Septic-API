0054b643ddf7c8a4a570c34e8bb5dc82d5371468
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-07-05 11:39:49 -0400

    test: start testhole post test

---
+++ spec/requests/api/v1/testhole_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+Rspec.describe "testhole requests" do
+  describe 'test hole post requests' do
+    it 'posts a new test hole' do
+      prop = Property.create!(
+              city: "Cumberland",
+              street: "Sun Valley Dr",
+              num: "126",
+              bedrooms: "3")
+      data = {
+              "property_id": "#{prop.id}",
+              "shwt": "36",
+              "notes": "large tree nearby",
+              "Date": "7/5/2022",
+              "inspector_present": "true",
+              "ledge_depth": "999"
+              }
+              headers = { 'CONTENT_TYPE' => 'application/json', "Accept" => 'application/json'}
+              post '/api/v1/testholes', headers: headers, params: JSON.generate(data)
+              reply = JSON.parse(response.body, symbolize_names: true)
+
+    end
+  end
+end
